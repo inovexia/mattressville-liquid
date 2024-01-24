abce9b8f3d76b1abd4a13f93d9e357c8a626e7cb
4nkit-5hukla <ankit5hukla@hotmail.com>
2024-01-24 22:11:03 +0530

    updates

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  $(document).ready(function () {
-    console.log('ready');
-    $('.dismiss-extended-bar').click(function () {
-      $(this).parents('.extended-bar').remove();
-    });
-  });
-})(jQuery);
