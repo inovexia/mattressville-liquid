e602f037026eda48dd75230e25ab352babed3e77
Asif Aziz <asifaziz01@users.noreply.github.com>
2024-05-08 15:07:20 +0530

    Fixed some issues with swiper slider conflicts on Homepage and PDP

---
+++ assets/invx-scripts-pdp.js
@@ -0,0 +1,28 @@
+let $lgSwiper = document.getElementById('lg-swiper');
+var swiper = new Swiper('.mySwiperSmall', {
+    spaceBetween: 10,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper('.mySwiperMain', {
+    spaceBetween: 10,
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+    swiper: swiper,
+    },
+    // Init lightGallery ince swiper is initilized
+    on: {
+    init: function () {
+        const lg = lightGallery($lgSwiper);
+
+        // Before closing lightGallery, make sure swiper slide
+        // is aligned with the lightGallery active slide
+        $lgSwiper.addEventListener('lgBeforeClose', () => {
+        swiper.slideTo(lg.index, 0);
+        });
+    },
+    },
+});
