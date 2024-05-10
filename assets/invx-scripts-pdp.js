3144d8e68ab2aec5d7b0bfb21f2b4e45005828ad
Asif Aziz <asifaziz01@users.noreply.github.com>
2024-05-10 10:42:29 +0530

    Fixed home slider navigation buttons

--- assets/invx-scripts-pdp.js
+++ assets/invx-scripts-pdp.js
@@ -7,22 +7,22 @@ var swiper = new Swiper('.mySwiperSmall', {
 var swiper2 = new Swiper('.mySwiperMain', {
     spaceBetween: 10,
     navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     thumbs: {
-    swiper: swiper,
+        swiper: swiper,
     },
     // Init lightGallery ince swiper is initilized
     on: {
-    init: function () {
-        const lg = lightGallery($lgSwiper);
+        init: function () {
+            const lg = lightGallery($lgSwiper);
 
-        // Before closing lightGallery, make sure swiper slide
-        // is aligned with the lightGallery active slide
-        $lgSwiper.addEventListener('lgBeforeClose', () => {
-        swiper.slideTo(lg.index, 0);
-        });
-    },
+            // Before closing lightGallery, make sure swiper slide
+            // is aligned with the lightGallery active slide
+            $lgSwiper.addEventListener('lgBeforeClose', () => {
+                swiper.slideTo(lg.index, 0);
+            });
+        },
     },
 });
